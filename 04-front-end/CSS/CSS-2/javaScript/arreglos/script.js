b7f0503c3f49a5e2250eb15daff1f83fa9101897
jose orlando <joseospina15@hotmail.com>
2023-06-29 17:25:53 -0500

    se suben todas las carpetas utilizadas en clase

---
+++ 04-front-end/CSS/CSS-2/javaScript/arreglos/script.js
@@ -0,0 +1,15 @@
+var xiaomi = {
+  marca: "xiaomi redmi note 11",
+  camaras: 4,
+  macro: true,
+  flash: true,
+  cargaRapida: false,
+  camaraFrontal: true,
+  faceId: true,
+  lectorHuella: true,
+
+  Encender: function () {
+    return "esta encendido";
+  },
+  apagar: function () {},
+};
---
+++ 04-front-end/CSS/CSS-2/javaScript/tipos-de-datos/script.js
@@ -0,0 +1,20 @@
+
+//tipos de datos primitivos
+ 37 // Number;
+boolean // true ;
+boolean // false;
+String // "cadena de caracteres"
+undefined;
+null;
+
+//tipos de datos objetos
+
+  // [1, 3, hola, epa]  array
+
+
+var nombre = "jose orlando";
+
+//esto me permite mostrar algo por consola
+console.log('hola mi nombre es:' + nombre );
+
+
