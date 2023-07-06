c09bb2c0f29a4e8949c69e93a66372e4c8dcb7f0
jose orlando <joseospina15@hotmail.com>
2023-07-06 17:45:37 -0500

    se sube archivo donde se trabajo funciones

---
+++ 04-front-end/CSS/CSS-2/javaScript/funciones/funciones-II/ejercicio-1/script.js
@@ -0,0 +1,11 @@
+// function agregarNombre(arreglo, nombre) {
+//   arreglo.push(nombre);
+//   return arreglo;
+// }
+// const arreglo = ["jose", "marco", "fabian", "lalo"];
+// console.log(agregarNombre(arreglo, "monster"));
+
+function devolverString(str) {
+  return str;
+}
+console.log(devolverString("jose orlando"));
